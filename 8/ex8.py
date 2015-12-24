0567d0fb647f4fa15de83e19eae44fc46c14c7f2
Super Tiger <SuperTigerPlusPlus@gmail.com>
2015-12-24 12:46:36 +0100

    Completed excercise 8

---
+++ 8/ex8.py
@@ -0,0 +1,13 @@
+formatter = "%r %r %r %r"
+
+print formatter % (1, 2, 3, 4)
+print formatter % ("one", "two", "three", "four")
+print formatter % (True, False, False, True)
+print formatter % (formatter, formatter, formatter, formatter)
+print formatter % (
+	"I had this thing.",
+	"That you could type up right.",
+	# Double quotes are generated, because a single quote character is in the string
+	"But it didn't sing.",
+	"So I said goodnight."
+)
