45b9ff2c28604e1b624b3971b0f668da8bcfe3be
Super Tiger <SuperTigerPlusPlus@gmail.com>
2015-12-24 13:19:36 +0100

    Completed excercise 9

---
+++ 9/ex9.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Here's some new strang stuff, remember type it exactly
+
+days = "Mon Die Mit Don Fre Sam Son"
+# Okay, the byte code for ä looks correct. Okay, got it. The output is scrambled
+# because of the encoding used by windows. See: http://stackoverflow.com/questions/1259084/what-encoding-code-page-is-cmd-exe-using
+# Too lazy to fix it at the moment. 
+months = "Jan\nFeb\nMär\nApr\nMai\nJun\nJul\nAug\nSep\nOkt\nNov\nDez"
+
+print "Here are the days: ", days
+print "Here are the months: ", months
+
+print """
+There's something going on here.
+With the three double-quotes.
+We'll be ablte to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+"""
